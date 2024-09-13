8a22ad47fcf18558001fdd05b874cc3ac43f1d05
André Luiz <silva.ala82@gmail.com>
2024-09-12 22:28:45 -0300

    fix: removed some libs until security patch corrects them to secure use

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,38 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './application/users/users.module';
-import { AuthModule } from './application/auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './interfaces/http/guards/auth-guard';
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { ConfigModule } from '@nestjs/config';
-import { BoardsModule } from './application/board/boards.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./application/users/users.module";
+import { AuthModule } from "./application/auth/auth.module";
+import { APP_GUARD } from "@nestjs/core";
+import { AuthGuard } from "./interfaces/http/guards/auth-guard";
+// import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+import { ConfigModule } from "@nestjs/config";
+import { BoardsModule } from "./application/board/boards.module";
 
 @Module({
-  imports: [UsersModule, BoardsModule, AuthModule,
+  imports: [
+    UsersModule,
+    BoardsModule,
+    AuthModule,
     ConfigModule.forRoot({
-      isGlobal: true
+      isGlobal: true,
     }),
-    ThrottlerModule.forRoot([{
-      ttl: 60000,
-      limit: 15
-    }])
+    // ThrottlerModule.forRoot([{
+    //   ttl: 60000,
+    //   limit: 15
+    // }])
   ],
   controllers: [AppController],
-  providers: [AppService,
+  providers: [
+    AppService,
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
     },
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard
-    }
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: ThrottlerGuard
+    // }
   ],
 })
-export class AppModule { }
+export class AppModule {}
