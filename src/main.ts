c906b2620c5b330a338886353bbfc2e5f0e8f271
André Luiz <silva.ala82@gmail.com>
2024-09-17 13:42:50 -0300

    chore: add swagger to document routes and impements its configuration

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import helmet from 'helmet';
-import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import helmet from 'helmet'
+import { ConfigService } from '@nestjs/config'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
   app.use(helmet())
+
+  const config = new DocumentBuilder()
+    .setTitle('Kanban_Board')
+    .setDescription('Use this doc to test Kanban_Board backend api routes')
+    .setVersion('0.1')
+    .addTag('kanban_board')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('api', app, document, {
+    jsonDocumentUrl: 'api/json',
+  })
+
   const configService = app.get(ConfigService)
-  await app.listen(configService.get('PORT'));
+
+  await app.listen(configService.get('PORT'))
+  console.log(`Application is running on: ${await app.getUrl()}`)
 }
-bootstrap();
+bootstrap()
