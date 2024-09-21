3654c266c35ae4a1d4fbfc7c80f147ed0afe67c3
André Luiz <silva.ala82@gmail.com>
2024-09-21 16:32:21 -0300

    refactor: adjust example to api swagger and remove ununcsed imports

--- src/interfaces/controllers/dtos/collumn/update-collumn-order-response-200.dto.ts
+++ src/interfaces/controllers/dtos/collumn/update-collumn-order-response-200.dto.ts
@@ -1,11 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger'
 
+class UpdateCollumnResponse {
+  @ApiProperty()
+  id: '1be77a42-ad88-4587-b4bc-fab9e4152013'
+
+  @ApiProperty()
+  name: 'collumn1'
+
+  @ApiProperty()
+  order: 0
+
+  @ApiProperty()
+  board_id: '625f900d-340c-4d3e-acc0-cb2bf3c3d69c'
+}
+
 export class UpdateCollumnOrderResponse200 {
   @ApiProperty()
-  collumn: {
-    id: string
-    name: string
-    order: number
-    board_id: string
-  }
+  data: UpdateCollumnResponse[]
 }
--- src/interfaces/controllers/dtos/collumn/update-collumn-order.dto.ts
+++ src/interfaces/controllers/dtos/collumn/update-collumn-order.dto.ts
@@ -1,17 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { Type } from 'class-transformer'
 import {
-  ArrayContains,
-  ArrayNotEmpty,
   IsArray,
   IsNotEmpty,
   IsNumber,
   IsString,
-  Validate,
   ValidateNested,
-  ValidationArguments,
-  ValidatorConstraint,
-  ValidatorConstraintInterface,
 } from 'class-validator'
 
 class CollumnOrderParam {
@@ -31,7 +25,7 @@ export class UpdateCollumnOrderDTO {
   @Type(() => CollumnOrderParam)
   @ApiProperty({
     example:
-      '[{id: abc-abc-abc-abc, order:0 }, {id: abc-abc-abc-abc, order:1 }, {id: abc-abc-abc-abc, order:2 }]',
+      '[{"id": "1be77a42-ad88-4587-b4bc-fab9e4152013", order:0 }, {"id": "db61725a-01c9-44e0-a6b4-f1e3f6ece162", order:1 }, {"id": "db3c7c45-2db4-4886-9479-56f3680f7b4d", order:2 }]',
     description: 'An array with the new collumn order',
   })
   collumnOrder: CollumnOrderParam[]
